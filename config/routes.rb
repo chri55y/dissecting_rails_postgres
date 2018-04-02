5187564a5dfc4ce92cfb039df6a61050c53a4784
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-04-02 12:24:44 -0500

    root to pages#home (and get rid of get pages/home route)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
   resources :portfolios
+
   # TO DO: customize so that these 'pages/' isn't repeated throughout
-  get 'pages/home'
-
+  # get 'pages/home'
   get 'pages/about'
-
   get 'pages/contact'
 
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'pages#home'
+
 end
