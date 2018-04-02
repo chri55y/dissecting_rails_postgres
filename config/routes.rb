ee3defc6feca220d850851ffc87e5bfa86387ea4
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-04-02 13:00:27 -0500

    ***CUSTOMIZE RESOURCEFUL Routes*** to use singular 'portfolio' for show action
    do have to modify views (or any other links to portfolios#show) to
    custom prefix (now portfolio_show_ instead of just portfolio_—which
    carried down from update action in rake routes)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  # we want to customize to use singular noun for show action (makes more sense grammatically)
+  # except breaks portfolio_ helper prefix, so we have to add as: 'portfolio_show'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  # Pages
   get 'about',    to: 'pages#about'
   get 'about-me', to: 'pages#about'
   get 'contact',  to: 'pages#contact'
