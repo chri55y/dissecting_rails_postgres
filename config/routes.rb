1919cb53530040d7231aeb595916289807a5daf9
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-04-02 12:41:23 -0500

    modify about and contact so "pages/" is not part of the path

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   resources :portfolios
 
-  # TO DO: customize so that these 'pages/' isn't repeated throughout
-  # get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about',    to: 'pages#about'
+  get 'about-me', to: 'pages#about'
+  get 'contact',  to: 'pages#contact'
 
   resources :blogs
 
