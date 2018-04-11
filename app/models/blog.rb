bdb0aa2c7abb731796702c3476fd898a5b6ac596
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-04-10 21:16:40 -0500

    define enum for status in model. tested in console

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
+
+  enum status: { draft: 0, published: 1 }
+  # this will allow us to avoid using 1s and 0s directly
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
